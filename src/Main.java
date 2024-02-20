bd5ed33fbb49919db2e99e0329d79fe806fadb75
Dylan <dm5messerly@gmail.com>
2024-02-20 12:03:26 -0600

    Initial

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame g_win = new JFrame(); //gameplay window
+
+        g_win.setBounds(10, 10, 700, 600);
+        g_win.setTitle("Breakout Ball");
+        g_win.setResizable(false);
+        g_win.setVisible(true);
+        g_win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        g_win.setVisible(true);
+
+    }
+
+}
